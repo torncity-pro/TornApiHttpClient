8190137fecb6d41ef0e7fb87e400e880d2f61fa3
tornapi <admin@torncity.pro>
2020-12-18 18:41:26 -0800

    Adds util
    Adds a util for converting between dates and timestamps

---
+++ TornApiHttpClient/Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TornApiHttpClient.Extensions
+{
+    public static class Util
+    {
+        public static readonly DateTime UTCDate = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        public static DateTime TimestampToDateTime(this double timestamp)
+        {
+            return UTCDate.AddSeconds(timestamp);
+        }
+
+        public static DateTime TimestampToDateTime(this int timestamp)
+        {
+            return UTCDate.AddSeconds(timestamp);
+        }
+
+        public static double DateTimeToTimestamp(this DateTime datetime)
+        {
+            return (datetime - UTCDate).TotalSeconds;
+        }
+    }
+}
