5e43a37cd1b6ddb6bb5bc2e530bf057f52fec8a9
tornapi <admin@torncity.pro>
2021-05-20 09:39:35 -0700

    Quick fix
    Updates to DateTime.UnixEpoch instead of creating it myself.

--- TornApiHttpClient/Util.cs
+++ TornApiHttpClient/Util.cs
@@ -4,20 +4,19 @@ namespace TornApiHttpClient.Extensions
 {
     public static class Util
     {
-        public static readonly DateTime UTCDate = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
         public static DateTime TimestampToDateTime(this double timestamp)
         {
-            return UTCDate.AddSeconds(timestamp);
+            return DateTime.UnixEpoch.AddSeconds(timestamp);
         }
 
         public static DateTime TimestampToDateTime(this int timestamp)
         {
-            return UTCDate.AddSeconds(timestamp);
+            return DateTime.UnixEpoch.AddSeconds(timestamp);
         }
 
         public static double DateTimeToTimestamp(this DateTime datetime)
         {
-            return (datetime - UTCDate).TotalSeconds;
+            return (datetime - DateTime.UnixEpoch).TotalSeconds;
         }
     }
 }
